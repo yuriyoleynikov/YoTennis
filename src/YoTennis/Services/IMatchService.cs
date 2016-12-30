af08a9a232147e4d15f3b8dc265941d1bac09723
yuriyoleynikov <yuriyoleynikov.ru@gmail.com>
2016-12-30 13:21:03 +0300

    Implement UI

---
+++ src/YoTennis/Services/IMatchService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YoTennis.Models;
+
+namespace YoTennis.Services
+{
+    public interface IMatchService
+    {
+        Task AddEvent(GameEvent gameEvent);
+        Task<State> GetState();
+    }
+}
---
+++ src/YoTennis/Services/InMemoryMatchService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YoTennis.Models;
+
+namespace YoTennis.Services
+{
+    public class InMemoryMatchService : IMatchService
+    {
+        private GameModel _gameModel = new GameModel();
+
+        public async Task AddEvent(GameEvent gameEvent)
+        {
+            _gameModel.AddEvent(gameEvent);
+        }
+
+        public async Task<State> GetState()
+        {
+            return _gameModel.CurrentState;
+        }
+    }
+}
