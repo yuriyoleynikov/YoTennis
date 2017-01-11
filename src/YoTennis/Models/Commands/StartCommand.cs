53a0273c3404e4714175e04d4cfd79ce94695d26
Igor Oleinikov <igor@oleinikov.ru>
2017-01-11 09:46:18 -0800

    Improve views by breaking down to partial views. - StartCommand doesn't depend on MatchSetting - View-specific model with form - Annotations and validation

--- src/YoTennis/Models/Commands/StartCommand.cs
+++ src/YoTennis/Models/Commands/StartCommand.cs
@@ -1,14 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace YoTennis.Models.Commands
 {
     public class StartCommand
     {
-        public MatchSettings MatchSettings { get; set; }
+        [Required, Display(Name = "First Player Name")]
         public string FirstPlayer { get; set; }
+
+        [Required, Display(Name = "Second Player Name")]
         public string SecondPlayer { get; set; }
+
+        [Required, Display(Name = "Sets for win")]
+        public int SetsForWin { get; set; } = 2;
+
+        [Required, Display(Name = "Tiebreak")]
+        public bool TiebreakFinal { get; set; } = false;
+
+        [Required, Display(Name = "Games in a set")]
+        public int GamesInSet { get; set; } = 6;
+        public int PointsInGame { get; set; } = 4;
+        public int PointsInTiebreak { get; set; } = 7;
+
     }
 }
