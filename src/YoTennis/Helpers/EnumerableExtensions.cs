f098047bfc564fe56049c87ed4e617fc9a6b57f8
yuriyoleynikov <yuriyoleynikov.ru@gmail.com>
2017-06-14 14:47:54 +0300

    Initial class EnumerableExtensions

---
+++ src/YoTennis/Helpers/EnumerableExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YoTennis.Helpers
+{
+    internal static class EnumerableExtensions
+    {
+        public static IEnumerable<T> WithElement<T>(this IEnumerable<T> source, T element)
+        {
+            //
+            yield break;
+        }
+
+        public static IEnumerable<T> WithoutElement<T>(this IEnumerable<T> source, T element)
+        {
+            //
+            yield break;
+        }
+    }
+}
