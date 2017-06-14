a0cbbb5dc9824fb700761b6611491f061a292b90
yuriyoleynikov <yuriyoleynikov.ru@gmail.com>
2017-06-14 16:56:08 +0300

    -EnumerableExtensions realisation - Add tests for EnumerableExtensions

--- src/YoTennis/Helpers/EnumerableExtensions.cs
+++ src/YoTennis/Helpers/EnumerableExtensions.cs
@@ -5,17 +5,23 @@ using System.Threading.Tasks;
 
 namespace YoTennis.Helpers
 {
-    internal static class EnumerableExtensions
+    public static class EnumerableExtensions
     {
         public static IEnumerable<T> WithElement<T>(this IEnumerable<T> source, T element)
         {
-            //
+            foreach (T t in source)
+                yield return t;
+
+            yield return element;
             yield break;
         }
 
         public static IEnumerable<T> WithoutElement<T>(this IEnumerable<T> source, T element)
         {
-            //
+            foreach (T t in source)
+                if (!element.Equals(t))
+                    yield return t;
+
             yield break;
         }
     }
---
+++ tests/YoTennis.Tests/Test/EnumerableExtensionsTests.cs
@@ -0,0 +1,61 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using YoTennis.Controllers;
+using YoTennis.Helpers;
+
+namespace YoTennis.Tests.Test
+{
+    public class EnumerableExtensionsTests
+    {
+        [Fact]
+        public void Test_WithElement_String()
+        {
+            IEnumerable<string> sequence = new List<string> { "one", "two" };
+            sequence.WithElement("three");
+            sequence.Should().BeEquivalentTo(new List<string> { "one", "two", "three" });
+        }
+
+        [Fact]
+        public void Test_WithElement_Int()
+        {
+            IEnumerable<int> sequence = new List<int> { 1, 2 };
+            sequence.WithElement(3);
+            sequence.Should().BeEquivalentTo(new List<int> { 1, 2, 3 });
+        }
+
+        [Fact]
+        public void Test_WithoutElement_String()
+        {
+            IEnumerable<string> sequence = new List<string> { "one", "two" };
+            sequence.WithoutElement("two");
+            sequence.Should().BeEquivalentTo(new List<string> { "one" });
+        }
+
+        [Fact]
+        public void Test_WithoutElement_Int()
+        {
+            IEnumerable<int> sequence = new List<int> { 1, 2 };
+            sequence.WithoutElement(2);
+            sequence.Should().BeEquivalentTo(new List<int> { 1 });
+        }
+
+        [Fact]
+        public void Test_Null_WithElement_String()
+        {
+            IEnumerable<string> sequence = null;
+            sequence.WithElement("one");
+            sequence.Should().BeEquivalentTo(new List<string> { "one" });
+        }
+
+        [Fact]
+        public void Test_Null_WithElement_Int()
+        {
+            IEnumerable<string> sequence = null;
+            sequence.WithElement(1);
+            sequence.Should().BeEquivalentTo(new List<int> { 1 });
+        }
+    }
+}
